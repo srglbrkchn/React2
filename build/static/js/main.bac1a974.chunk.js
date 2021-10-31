72e8e0730cc162fc0b88561d75be6a9c6a369d0c
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-10-31 08:56:58 +0100

    Final commit. Insert my 3 most favorite animes into JSX file.

---
+++ build/static/js/main.bac1a974.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpjsx-code-challenge"]=this["webpackJsonpjsx-code-challenge"]||[]).push([[0],{3:function(e,l,a){"use strict";a.r(l);var n=a(0),t=a.n(n),c=a(2);a.n(c).a.render(t.a.createElement("div",null,t.a.createElement("h1",null,"My favorite animes"),t.a.createElement("ul",null,t.a.createElement("li",null,"Black Butler"),t.a.createElement("li",null,"Gangsta"),t.a.createElement("li",null,"Ayakashi: Japanese Classic Horror"))," "),document.getElementById(root))}},[[3,1,2]]]);
+//# sourceMappingURL=main.bac1a974.chunk.js.map
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(<div>
+    <h1>My favorite animes</h1>
+    <ul>
+        <li>Black Butler</li>
+        <li>Gangsta</li>
+        <li>Ayakashi: Japanese Classic Horror</li>
+    </ul> </div>, document.getElementById("root"));
